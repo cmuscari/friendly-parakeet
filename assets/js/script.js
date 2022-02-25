0c016a24ae3f528aa767b692121b0bf936d4b9da
Cortnie <cortniemuscari@gmail.com>
2022-02-24 22:26:10 -0600

    started writing generate password function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,48 @@
-// Assignment code here
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "1234567890";
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
 
-
-
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+
+
+
+
+// GENERATE PASSWORD
+var generatePassword = function () {
+
+  // Function to determine password LENGTH
+  var choosePasswordLength = function () {
+    var passwordLength = prompt("How long would you like your password to be? \nChoose between 8-128 characters.");
+      
+      if (passwordLength === null || passwordLength === "" || passwordLength < 8 || passwordLength > 128) {
+        alert("Please choose a number between 8-128!");
+        choosePasswordLength();
+      }
+      else {
+        alert("You have chosen to make your password " + passwordLength + " characters long.");
+        return passwordLength;
+      }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -15,6 +52,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
