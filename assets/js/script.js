242061250f165f1a759d86e8df64ae1134939158
Cortnie <cortniemuscari@gmail.com>
2022-02-24 23:05:49 -0600

    worked on password length function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,26 +8,29 @@ var generateBtn = document.querySelector("#generate");
 
 
 
-
-
-// GENERATE PASSWORD
+// GENERATE PASSWORD FUNCTION
 var generatePassword = function () {
+  choosePasswordLength();
 
-  // Function to determine password LENGTH
-  var choosePasswordLength = function () {
-    var passwordLength = prompt("How long would you like your password to be? \nChoose between 8-128 characters.");
-      
-      if (passwordLength === null || passwordLength === "" || passwordLength < 8 || passwordLength > 128) {
-        alert("Please choose a number between 8-128!");
-        choosePasswordLength();
-      }
-      else {
-        alert("You have chosen to make your password " + passwordLength + " characters long.");
-        return passwordLength;
-      }
-  }
+return xxx;
 }
 
+
+
+
+// Function to determine password LENGTH
+var choosePasswordLength = function () {
+  var passwordLength = prompt("How long would you like your password to be? \nChoose between 8-128 characters.");
+    
+    if (passwordLength === null || passwordLength === "" || passwordLength < 8 || passwordLength > 128) {
+      alert("Please choose a number between 8-128!");
+      choosePasswordLength();
+    }
+    else {
+      alert("You have chosen to make your password " + passwordLength + " characters long.");
+      return passwordLength;
+    }
+}
 
 
 
